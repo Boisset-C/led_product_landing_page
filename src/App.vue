aa75321e534f0ca849be56dc66adc869667910c7
Cosme <boissetcosme@gmail.com>
2021-04-04 18:22:07 -0400

    last commit

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,47 @@
 <template>
-<div id=#app>
+  <div id="#app">
+    <section class="hero">
+      <hero-section></hero-section>
+    </section>
 
-<section class="hero">
-  <hero-section></hero-section>
-</section>
+    <section class="blurb">
+      <three-blurbs></three-blurbs>
+    </section>
 
-<section class="blurb">
-  <three-blurbs></three-blurbs>
-</section>
+    <section class="social-proof">
+      <social-proof></social-proof>
+    </section>
 
-<section class="social-proof">
-  <social-proof></social-proof>
-</section>
+    <section class="features">
+      <features-part-one></features-part-one>
+      <features-part-two></features-part-two>
+    </section>
 
-<section class="features">
-  <features-part-one></features-part-one>
-  <features-part-two></features-part-two>
-</section>
+    <section class="cta">
+      <call-to-action></call-to-action>
+    </section>
 
-
-<section class="cta">
-  <call-to-action></call-to-action>
-</section>
-
-<section class="footer">
-  <footer-section></footer-section>
-</section>
-
-</div>
+    <section class="footer">
+      <footer-section></footer-section>
+    </section>
+  </div>
 </template>
 
 <script>
-
-
 export default {
-  name: 'App',
-  components:{
-
-  },
-}
+  name: "App",
+  components: {},
+};
 </script>
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@200&display=swap');
+@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@200&display=swap");
 
 * {
   margin: 0.5rem;
 
   font-family: Poppins;
-  box-sizing: border-box
+  box-sizing: border-box;
 }
 #app {
   display: grid;
@@ -57,18 +50,18 @@ export default {
   height: 100vh;
 }
 .social-proof {
-  background: #52525B;
+  background: #52525b;
 }
 .hero {
-  background: #52525B;
+  background: #52525b;
 }
 
 .blurb {
-background: white;
+  background: white;
 }
 .cta {
-background-image: url('./assets/rectangle.svg');
-height: 350px;
+  background-image: url("./assets/rectangle.svg");
+  height: 350px;
 }
 .features {
   display: flex;
@@ -77,6 +70,6 @@ height: 350px;
   background: #f2f2f2;
 }
 .footer {
-  background: #52525B;
+  background: #52525b;
 }
 </style>
--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -1,56 +1,55 @@
 <template>
   <the-nav-bar></the-nav-bar>
-<div class="container">
-  <h1>Bring atmosphere to your Zoom meetings</h1>
-  <p>LightHOUSE is a customizable desk lamp so deal with it</p>
-  <button class="btn-styled">LEARN MORE</button>
-  <button class="btn-flat">LEARN MORE</button>
-
-
-</div>
+  <div class="container">
+    <h1>Bring atmosphere to your Zoom meetings</h1>
+    <p>LightHOUSE is a customizable desk lamp so deal with it</p>
+    <button class="btn-styled">LEARN MORE</button>
+    <button class="btn-flat">LEARN MORE</button>
+  </div>
 </template>
 
 <script>
-import TheNavBar from '../layout/TheNavBar.vue';
+import TheNavBar from "../layout/TheNavBar.vue";
 
 export default {
   components: {
     TheNavBar,
-  }
-  
-}
+  },
+};
 </script>
 
 <style scoped>
 .container {
   flex-wrap: wrap;
-  background: #52525B;
+  background: #52525b;
   align-items: center;
   padding-bottom: 2rem;
 }
 button {
-padding: .5rem 2rem .5rem 2rem;
+  padding: 0.5rem 2rem 0.5rem 2rem;
 }
 .btn-styled {
-margin-top: 3rem;
-margin-left: 20%;
-border: 0.5px solid rgba(255, 255, 255, 0.04);
-box-sizing: border-box;
-box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.11), -2px -2px 4px rgba(255, 255, 255, 0.11), 9px 9px 12px rgba(0, 0, 0, 0.11), -9px -9px 12px rgba(255, 255, 255, 0.11);
-border-radius: 33px;
-font-size: 1rem;
+  margin-top: 3rem;
+  margin-left: 20%;
+  border: 0.5px solid rgba(255, 255, 255, 0.04);
+  box-sizing: border-box;
+  box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.11),
+    -2px -2px 4px rgba(255, 255, 255, 0.11), 9px 9px 12px rgba(0, 0, 0, 0.11),
+    -9px -9px 12px rgba(255, 255, 255, 0.11);
+  border-radius: 33px;
+  font-size: 1rem;
 
-background-color: #52525B;
-border-color: #52525B;
-border-style: none;
-color: white;
+  background-color: #52525b;
+  border-color: #52525b;
+  border-style: none;
+  color: white;
 }
 
 .btn-flat {
-background-color: #52525B;
-border-style: none;
-color: white;
-font-size: 1rem;
+  background-color: #52525b;
+  border-style: none;
+  color: white;
+  font-size: 1rem;
 }
 
 h1 {
@@ -69,12 +68,12 @@ p {
   margin-left: 20%;
 }
 #circle {
-    display: flex;
-    margin-left: 50%;
-    background: #c4c4c4;
-    border-radius: 50%;
-    width: 4rem;
-    height: 4rem;
-    margin-top: 2rem;
+  display: flex;
+  margin-left: 50%;
+  background: #c4c4c4;
+  border-radius: 50%;
+  width: 4rem;
+  height: 4rem;
+  margin-top: 2rem;
 }
 </style>
